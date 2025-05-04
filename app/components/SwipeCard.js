d779c6f9b8128a396c80d93312352727f55cde94
Ekkiddle <ekkiddle@gmail.com>
2025-05-04 12:28:09 -0600

    Swipe Card task Complete

--- app/components/SwipeCard.js
+++ app/components/SwipeCard.js
@@ -2,6 +2,7 @@
 import DraggableContainer from "./DraggableDiv";
 import { useRef, useState } from "react";
 import localFont from 'next/font/local'
+import Image from "next/image";
 
 const digi = localFont({ src: '../fonts/time.ttf' })
 
@@ -9,7 +10,26 @@ export default function CardTask() {
     const containerRef = useRef(null);
     const cardRef = useRef(null)
     const swipeAreaRef = useRef(null);
-    const [message, setMessage] = useState('Please Insert Card')
+    const [message, setMessage] = useState('Please Insert Card');
+    const [tempMessage, setTempMessage] = useState('');
+
+    const [swiping, setSwiping] = useState(false);
+    const [swiped, setSwiped] = useState(false);
+
+    const [start, setStart] = useState(null);
+    const [startTime, setStartTime] = useState(null);
+    const [timeoutId, setTimeoutId] = useState(null);
+
+    function showTemporaryMessage(newMessage, delay = 1500) {
+        setMessage(newMessage);
+        if (timeoutId) clearTimeout(timeoutId); // Clear old timeout
+        const id = setTimeout(() => {
+            if (message != "Swipe Card" || message != 'Swiping...'){
+                setMessage("Please Insert Card");
+            }
+        }, delay);
+        setTimeoutId(id);
+    }    
 
     function isOverlapping(obj1, obj2) {
         // Get bounding rectangles for both objects
@@ -22,15 +42,86 @@ export default function CardTask() {
       
         // If both horizontal and vertical overlap, then they are overlapping
         return horizontalOverlap && verticalOverlap;
-      }
+    }
+
+      function evaluateSwipe(swipeRight) {
+        const swipeArea = swipeAreaRef.current.getBoundingClientRect();
+        const card = cardRef.current.getBoundingClientRect();
+    
+        const swipeDuration = (Date.now() - startTime) / 1000; // in seconds
+        const swipeDistance = Math.abs(card.left - start); // pixel distance
+        const swipeThreshold = swipeArea.width * 0.5;
+    
+        if (!swipeRight) {
+            setTempMessage("Wrong Direction");
+            return;
+        }
+    
+        if (swipeDistance < swipeThreshold) {
+            console.log(swipeDistance, swipeThreshold)
+            setTempMessage("Didn't swipe full length");
+            return;
+        }
+    
+        if (swipeDuration < 1.5) {
+            console.log(swipeDuration);
+            setTempMessage("Too fast");
+            return;
+        }
+    
+        if (swipeDuration > 2.2) {
+            setTempMessage("Too slow");
+            return;
+        }
+    
+        setTempMessage("Success!");
+        console.log("Success")
+    }
+    
       
 
     const dragFunction = () => {
         if(isOverlapping(cardRef, swipeAreaRef)){
-            setMessage('Swipe Card')
+            if (swiping && start) {
+                if (!swiped){
+                    const card = cardRef.current.getBoundingClientRect()
+                    const swipeArea = swipeAreaRef.current.getBoundingClientRect()
+                    if (((card.right > (swipeArea.right - swipeArea.width * 0.08)) && (start < card.left))){
+                        setMessage("Remove Card");
+                        setSwiped(true);
+                        console.log("Swipe right")
+                        evaluateSwipe(true);
+                    }
+                    if (((card.left < (swipeArea.left + swipeArea.width * 0.08)) && (start > card.left))){
+                        setMessage("Remove Card");
+                        setSwiped(true);
+                        console.log("Swipe left")
+                        evaluateSwipe(false);
+                    }
+                }
+                
+            } else {
+                if (!start){
+                    setStart(cardRef.current.getBoundingClientRect().left);
+                    setStartTime(Date.now());
+
+                }
+                const margin = swipeAreaRef.current.getBoundingClientRect().width * 0.1;
+                setMessage('Swipe Card')
+                if (start && !((start-margin) < cardRef.current.getBoundingClientRect().left && cardRef.current.getBoundingClientRect().left < (start+margin))){
+                    setSwiping(true)
+                    setMessage('Swiping..')
+                }
+            }
         }
         else{
-            setMessage("Please Insert Card")
+            if (start){
+                setStart(null);
+                setSwiping(false);
+                setSwiped(false);
+                setStartTime(null);
+                showTemporaryMessage(tempMessage);
+            }
         }
     }
 
@@ -59,9 +150,10 @@ export default function CardTask() {
             <div className="absolute w-full h-[30%] top-0 left-0 bg-gray-400 rounded-bl-4xl border-4 border-black z-20"
                 ref={swipeAreaRef}
             >
-                <div className={`w-[90%] mx-[5%] mt-2 bg-green-900 h-8 p-1 text-white ${digi.className}`}>
+                <div className={`w-[90%] mx-[5%] mt-2 bg-green-900 h-[29%] p-1 text-white ${digi.className}`}>
                     {message}
                 </div>
+                <img src="/swipe.svg" className="mx-1 h-[64%]" />
             </div>
             <div className="absolute w-full h-[30%] top-[2%] left-0 bg-gray-800 rounded-bl-4xl z-5" />
             <div className="absolute w-full h-[20%] top-[25%] left-0 bg-gray-700 z-3"/>
