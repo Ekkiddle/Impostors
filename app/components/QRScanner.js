84cc61c7f5e588e823e41d560e6ea18b14b71cbb
Ekkiddle <ekkiddle@gmail.com>
2025-06-12 12:27:24 -0600

    Forgot useState

--- app/components/QRScanner.js
+++ app/components/QRScanner.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Html5Qrcode } from 'html5-qrcode';
 
 export default function QRScanner({ onScan, onError }) {
